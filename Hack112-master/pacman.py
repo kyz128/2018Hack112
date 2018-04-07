9aee0344b33839a7aabe1714590a4a8018a05d28
lauren-tan <32051906+lauren-tan@users.noreply.github.com>
2018-04-07 11:44:34 -0400

    Update pacman.py

--- Hack112-master/pacman.py
+++ Hack112-master/pacman.py
@@ -1,13 +1,21 @@
 class Pacman(object):
 
-    def __init__(self, x, y, data):
-        self.x = x
-        self.y = y
+    def __init__(self, row, col, data):
+        self.x = data.cellSize/2 + data.cellSize*row
+        self.y = data.cellSize/2 + data.cellSize*col
         self.size = (data.cellSize*2/3)/2
-            
+         
+    def collideWithWalls(row, col, data):
+        for i in range(len(data.maze)):
+            for j in range(len(data.maze[0])):
+                if ((data.maze[i][j] == 1) and (row == i) and (col == j)):
+                    return True
+        return False
+        
     def move(self, dx, dy, data):
         if ((data.cellSize//2 < self.x + dx < data.cols-1*data.cellSize)
-            and (data.cellSize//2 < self.y + dy < data.rows*data.cellSize):
+            and (data.cellSize//2 < self.y + dy < data.rows*data.cellSize)
+            and not collideWithWalls(self.x//data.cellSize, self.y//data.cellSize, data)):
             self.x += dx
             self.y += dy
 
@@ -15,3 +23,7 @@ class Pacman(object):
         r = self.size
         canvas.create_oval(self.x-r, self.y-r, 
                            self.x+r, self.y+r, fill=color)
+
+
+
+
