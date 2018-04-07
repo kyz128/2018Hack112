8bbff91655e90dfb9abe64f693f1a171b41e9bfc
sarah33t <38145509+sarah33t@users.noreply.github.com>
2018-04-07 15:38:05 -0400

    Update monster.py

--- Hack112-master/monster.py
+++ Hack112-master/monster.py
@@ -1,51 +1,37 @@
 import random 
-def inBoard(x, y, data):
-    r = data.cellSize//2
-    nR = len(data.maze)
-    nC = len(data.maze[0])
-    return (r < x < ((data.cellSize * nC) - r)) and (r < y < ((data.cellSize * nR) - r))
+import copy
+
+def collideWithWalls(row, col, data):
+    for i in range(len(data.maze)):
+        for j in range(len(data.maze[0])):
+            if ((data.maze[i][j] == 1) and (row == i) and (col == j)):
+                return True
+    return False
 
 class Monster(object):
     all_monsters = []
     
     def __init__(self, data, color):
         self.color = color
-        self.x = random.randint(1, data.width - 1)
-        self.y = random.randint(1, data.height - 1)
-        self.r, self.c = self.x//data.cellSize, self.y//data.cellSize
+        self.size = data.cellSize
+        self.r, self.c = data.monster[0], data.monster[1]
         Monster.all_monsters.append(self)
     
     @staticmethod
     def draw(data, canvas):
         for monster in Monster.all_monsters:
-            canvas.create_oval(monster.x, monster.y, monster.x + data.cellSize,
-                            monster.y + data.cellSize, fill = monster.color)
+            r = monster.size
+            canvas.create_oval(monster.r * monster.size, monster.c * monster.size,
+                    (monster.r + 1) * monster.size, (monster.c + 1) * monster.size,
+                    fill = monster.color)
     
     @staticmethod
-    def move(data, direction):
+    def move(data, dr, dc):
     #moves monster
         if data.gameOver: return
 
         for monster in Monster.all_monsters:
-            temp = [monster.x, monster.y]
-            if direction == 'up':
-                temp[1] -= data.speedY
-            elif direction == 'down':
-                temp[1] += data.speedY
-            elif direction == 'left':
-                temp[0] -= data.speedX
-            elif direction == 'right':
-                temp[0] += data.speedX
-            
-            if not monster.collideWithWalls(monster.r, monster.c, data):
-            # #move the monster only if the next position is valid
-                monster.x, monster.y = temp[0], temp[1]
-    
-    @staticmethod
-    def collideWithWalls(row, col, data):
-        for i in range(len(data.maze)):
-            for j in range(len(data.maze[0])):
-                if ((data.maze[i][j] == 1) and (row == i) and (col == j) and
-                inBoard(row * data.cellSize, col * data.cellSize, data)):
-                    return True
-        return False
+            if ((0 < monster.x + dx < (data.cols - 1) * data.cellSize) and 
+                (0 < monster.y + dy < (data.rows - 1) * data.cellSize)):
+                monster.x += dx
+                monster.y += dy
