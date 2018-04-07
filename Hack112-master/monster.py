ece73346ec69e95c23068542848e2c604e5827d6
sarah33t <38145509+sarah33t@users.noreply.github.com>
2018-04-07 17:06:07 -0400

    Update monster.py

--- Hack112-master/monster.py
+++ Hack112-master/monster.py
@@ -32,3 +32,31 @@ class Monster(object):
             data.lives -= 1
             data.monsterCenter = 0, 1
             data.center = data.cols//2 - 1, data.rows//2 - 1
+
+            
+            
+import random 
+import copy
+
+class Monster(object):
+    all_monsters = []
+    
+    def __init__(self, data, color):
+        self.color = color
+        self.size = data.cellSize
+        self.r, self.c = data.monsterCenter[0], data.monsterCenter[1]
+        Monster.all_monsters.append(self)
+    
+    
+    def draw(self, data, canvas):
+        r = self.size
+        canvas.create_oval(self.r * self.size, self.c * self.size,
+                        (self.r + 1) * self.size, (self.c + 1) * self.size,
+                        fill = self.color)
+
+    def move(self, data, dr, dc):
+    #moves monster
+        if data.gameOver: return
+        if ((0 < self.r + dr < data.cols) and (1 < self.c + dc < data.rows)):
+                self.r += dr
+                self.c += dc
