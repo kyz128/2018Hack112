616412c63b16c533f6233d35c25efe5d61223f6b
lauren-tan <32051906+lauren-tan@users.noreply.github.com>
2018-04-07 11:53:46 -0400

    Update pacman.py

--- Hack112-master/pacman.py
+++ Hack112-master/pacman.py
@@ -1,19 +1,19 @@
+def collideWithWalls(row, col, data):
+    for i in range(len(data.maze)):
+        for j in range(len(data.maze[0])):
+            if ((data.maze[i][j] == 1) and (row == i) and (col == j)):
+                return True
+    return False
+    
 class Pacman(object):
 
     def __init__(self, row, col, data):
         self.x = data.cellSize/2 + data.cellSize*row
         self.y = data.cellSize/2 + data.cellSize*col
         self.size = (data.cellSize*2/3)/2
-         
-    def collideWithWalls(row, col, data):
-        for i in range(len(data.maze)):
-            for j in range(len(data.maze[0])):
-                if ((data.maze[i][j] == 1) and (row == i) and (col == j)):
-                    return True
-        return False
-        
+    
     def move(self, dx, dy, data):
-        if ((data.cellSize//2 < self.x + dx < data.cols-1*data.cellSize)
+        if ((data.cellSize//2 < self.x + dx < data.cols*data.cellSize)
             and (data.cellSize//2 < self.y + dy < data.rows*data.cellSize)
             and not collideWithWalls(self.x//data.cellSize, self.y//data.cellSize, data)):
             self.x += dx
